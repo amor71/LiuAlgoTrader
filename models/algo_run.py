67017197538588a5c803744713bbb54a215ad811
Amichay Oren <amichay@appcard.com>
2020-01-19 07:30:25 -0500

    saving end time

--- models/algo_run.py
+++ models/algo_run.py
@@ -9,16 +9,28 @@ class AlgoRun:
         self.environment = environment
         self.build = build
         self.parameters = parameters
+        self.algo_run_id = None
 
     async def save(self, db_connection):
         async with db_connection.transaction():
-            await db_connection.execute(
+            self.algo_run_id = await db_connection.fetchval(
                 """
                     INSERT INTO algo_run (algo_name, algo_env, build_number, parameters)
                     VALUES ($1, $2, $3, $4)
+                    RETURNING algo_run_id
                 """,
                 self.name,
                 self.environment,
                 self.build,
                 json.dumps(self.parameters),
+            )
+
+    async def update_end_time(self, db_connection):
+        async with db_connection.transaction():
+            self.algo_run_id = await db_connection.fetchval(
+                """
+                    UPDATE algo_run SET end_time = 'now()'
+                    WHERE algo_run_id
+                """,
+                self.algo_run_id,
             )
