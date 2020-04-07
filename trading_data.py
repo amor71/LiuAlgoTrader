e0623d3c389be596a45c446847ea6946d0624288
amor71 <amichay@sgeltd.com>
2020-04-07 10:45:35 +0300

    refactor code clean-up

--- trading_data.py
+++ trading_data.py
@@ -1,5 +1,5 @@
 """Global data shared during trading"""
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from alpaca_trade_api.entity import Order
 from asyncpg.pool import Pool
@@ -14,7 +14,7 @@ filename: str
 db_conn_pool: Pool
 
 strategies: List[Strategy] = []
-open_orders: Dict[str, (Order, str)] = {}
+open_orders: Dict[str, Tuple[Order, str]] = {}
 open_order_strategy: Dict[str, Strategy] = {}
 latest_cost_basis: Dict[str, float] = {}
 sell_indicators: Dict[str, Dict] = {}
