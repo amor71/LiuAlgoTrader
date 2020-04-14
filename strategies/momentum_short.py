13f0d02b45cfdfed0dc872886f38cf1fb7420f73
Amichay Oren <amichay@appcard.com>
2020-04-14 11:17:25 +0300

    1. change momentum_short entry strategy + better logging 2. improve logging when on exit 3. improve git logging

--- strategies/momentum_short.py
+++ strategies/momentum_short.py
@@ -7,18 +7,12 @@ from pandas import DataFrame as df
 from talib import MACD, RSI
 
 from common import config
-from common.tlog import tlog
 from common.market_data import prev_closes, volume_today
+from common.tlog import tlog
+from common.trading_data import (buy_indicators, latest_cost_basis,
+                                 open_order_strategy, open_orders,
+                                 sell_indicators, stop_prices, target_prices)
 from fincalcs.support_resistance import find_resistances, find_supports
-from common.trading_data import (
-    buy_indicators,
-    latest_cost_basis,
-    open_order_strategy,
-    open_orders,
-    sell_indicators,
-    stop_prices,
-    target_prices,
-)
 
 from .base import Strategy
 
@@ -29,7 +23,9 @@ class MomentumShort(Strategy):
     name = "momentum_short"
 
     def __init__(self, trading_api: tradeapi, data_api: tradeapi):
-        super().__init__(name=self.name, trading_api=trading_api, data_api=data_api)
+        super().__init__(
+            name=self.name, trading_api=trading_api, data_api=data_api
+        )
 
     async def create(self) -> None:
         await super().create()
@@ -60,7 +56,9 @@ class MomentumShort(Strategy):
             and close > high_15m
             and volume_today[symbol] > 30000
         ):
-            macds = MACD(minute_history["close"].dropna().between_time("9:30", "16:00"))
+            macds = MACD(
+                minute_history["close"].dropna().between_time("9:30", "16:00")
+            )
 
             macd = macds[0]
             macd_signal = macds[1]
@@ -77,7 +75,7 @@ class MomentumShort(Strategy):
                 # check RSI is high enough
                 rsi = RSI(minute_history["close"], 14)
                 tlog(f"[{self.name}]\t\tRSI for {symbol}={round(rsi[-1], 2)}")
-                if rsi[-1] > 75:
+                if rsi[-1] > 76:
                     tlog(f"[{self.name}]\t\tRSI for {symbol} is above signal")
                     return (
                         True,
@@ -92,7 +90,7 @@ class MomentumShort(Strategy):
 
     async def _find_target_stop_prices(
         self, symbol: str, close: float, minute_history: df, now: datetime
-    ) -> Tuple[Optional[float], Optional[float]]:
+    ) -> Tuple[Optional[float], Optional[float], Optional[float]]:
         supports = find_supports(
             symbol=symbol,
             strategy_name=self.name,
@@ -106,18 +104,15 @@ class MomentumShort(Strategy):
             minute_history=minute_history,
         )
         if supports is None or len(supports) == 0:
-            return None, None
+            return None, None, None
         if resistances is None or len(resistances) == 0:
-            return None, None
+            return None, None, None
 
         target_price = supports[-1]
         stop_price = resistances[-1]
         distance_to_stop = (stop_price - close) / close
 
-        if distance_to_stop < 0.02:
-            return None, None
-
-        return target_price, stop_price
+        return target_price, stop_price, distance_to_stop
 
     async def is_not_shortable(self, symbol: str) -> bool:
         asset = self.trading_api.get_asset(symbol)
@@ -144,14 +139,25 @@ class MomentumShort(Strategy):
             if not to_buy:
                 return False
 
-            target_price, stop_price = await self._find_target_stop_prices(
-                symbol=symbol, close=data.close, minute_history=minute_history, now=now,
+            (
+                target_price,
+                stop_price,
+                distance_to_stop,
+            ) = await self._find_target_stop_prices(
+                symbol=symbol,
+                close=data.close,
+                minute_history=minute_history,
+                now=now,
             )
             if target_price is None or stop_price is None:
                 return False
 
-            portfolio_value = float(self.trading_api.get_account().portfolio_value)
-            shares_to_buy = (portfolio_value * config.risk // data.close) - position
+            portfolio_value = float(
+                self.trading_api.get_account().portfolio_value
+            )
+            shares_to_buy = (
+                portfolio_value * config.risk // data.close
+            ) - position
             if shares_to_buy <= 0:
                 return False
 
@@ -160,8 +166,17 @@ class MomentumShort(Strategy):
                 return False
 
             tlog(
-                f"[{self.name}]\t\t\tSubmitting short sell for {shares_to_buy} shares of {symbol} at {data.close} target {target_price} stop {stop_price}"
+                f"[{self.name}]\t\t\tSubmitting short sell for {shares_to_buy} shares of {symbol} at {data.close} target {target_price} stop {stop_price} distance_to_stop {distance_to_stop}"
             )
+
+            indicators.update(
+                {
+                    "stop_price": stop_price,
+                    "target_price": target_price,
+                    "distance_to_stop": distance_to_stop,
+                }
+            )
+
             sell_indicators[symbol] = indicators
 
             try:
@@ -187,9 +202,9 @@ class MomentumShort(Strategy):
         elif await super().is_sell_time(now) and position < 0:
             # Check for liquidation signals
             rsi = RSI(minute_history["close"], 14)
-            movement = (data.close - latest_cost_basis[symbol]) / latest_cost_basis[
-                symbol
-            ]
+            movement = (
+                data.close - latest_cost_basis[symbol]
+            ) / latest_cost_basis[symbol]
 
             to_sell = False
             sell_reasons = []
@@ -198,17 +213,19 @@ class MomentumShort(Strategy):
                 sell_reasons.append("stopped")
             elif data.close <= target_prices[symbol]:
                 to_sell = True
-                sell_reasons.append(f"target")
+                sell_reasons.append("target")
             elif rsi[-1] <= 30.0:
                 to_sell = True
-                sell_reasons.append(f"low RSI")
+                sell_reasons.append("low RSI")
 
             if to_sell:
                 try:
                     buy_indicators[symbol] = {
                         "rsi": rsi[-5:].tolist(),
                         "movement": movement,
-                        "reasons": " AND ".join([str(elem) for elem in sell_reasons]),
+                        "reasons": " AND ".join(
+                            [str(elem) for elem in sell_reasons]
+                        ),
                     }
 
                     tlog(
