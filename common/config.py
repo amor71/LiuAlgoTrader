80eaf2f51790a5568846357244628fa8c73c7808
Amichay Oren <amichay@appcard.com>
2020-06-03 12:31:39 +0300

    wait 20 minutes instead of 15

--- common/config.py
+++ common/config.py
@@ -43,7 +43,7 @@ today_change_percent = 3.5
 group_margin = 0.02
 
 # trade times
-market_cool_down_minutes: int = 15
+market_cool_down_minutes: int = 20
 market_liquidation_end_time_minutes: int = 15
 market_open: datetime
 market_close: datetime
