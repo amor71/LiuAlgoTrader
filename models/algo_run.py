4f89c109fd204e042b57932e1d838d76fbd85286
Amichay Oren <amichay@appcard.com>
2020-06-20 20:42:58 +0300

    wip

--- models/algo_run.py
+++ models/algo_run.py
@@ -1,8 +1,9 @@
 import json
+from typing import Dict, List
 
 from asyncpg.pool import Pool
 
-from common import config, trading_data
+from common import config
 
 
 class AlgoRun:
@@ -22,7 +23,7 @@ class AlgoRun:
                     """,
                     self.strategy_name,
                     config.env,
-                    trading_data.build_label,
+                    config.build_label,
                     json.dumps(
                         {
                             "TRADE_BUY_WINDOW": config.trade_buy_window,
@@ -43,3 +44,31 @@ class AlgoRun:
                     end_reason,
                     self.run_id,
                 )
+
+    @classmethod
+    async def get_batches(
+        cls, pool: Pool = None, nax_batches: int = 30
+    ) -> Dict[str, List[str]]:
+        rc: Dict = {}
+        if not pool:
+            pool = config.db_conn_pool
+        async with pool.acquire() as con:
+            async with con.transaction():
+                rows = await con.fetch(
+                    """
+                        SELECT batch_id, algo_run_id, algo_name, algo_env, build_number, start_time
+                        FROM algo_run
+                        ORDER BY start_time DESC
+                        LIMIT $1
+                    """,
+                    nax_batches,
+                )
+
+                if rows:
+                    for row in rows:
+                        if row[0] not in rc:
+                            rc[row[0]] = [list(row.values())[1:]]
+                        else:
+                            rc[row[0]].append(list(row.values())[1:])
+
+        return rc
