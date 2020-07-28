6d8ecc425c37e22ab8ed47d74714ab812d9bca0b
Amichay Oren <amichay@appcard.com>
2020-07-28 21:44:37 +0300

    adding dragonfly candle and fixing spinning top

--- fincalcs/candle_patterns.py
+++ fincalcs/candle_patterns.py
@@ -19,24 +19,55 @@ def doji(open: float, close: float, high: float, low: float) -> bool:
     return close == open and low <= open - 0.01 and high >= open + 0.01
 
 
-def bull_spinning_top(
+def spinning_top(open: float, high: float, low: float, close: float) -> bool:
+    upper_shadow = high - close
+    lower_shadow = open - low
+    shadow_size = upper_shadow + lower_shadow
+    body_size = close - open if close > open else open - close
+    return (
+        shadow_size > 3 * body_size and 0.9 < upper_shadow / lower_shadow < 1.1
+    )
+
+
+def bullish_candle(open: float, high: float, low: float, close: float) -> bool:
+    upper_shadow = high - close
+    lower_shadow = open - low
+    shadow_size = upper_shadow + lower_shadow
+    body_size = close - open
+
+    return close > open + 0.02 and body_size > shadow_size * 1.2
+
+
+def bull_dragonfly_candle(
     open: float, high: float, low: float, close: float
 ) -> bool:
     upper_shadow = high - close
     lower_shadow = open - low
     shadow_size = upper_shadow + lower_shadow
+    body_size = close - open
+
     return (
-        high > close > open > low
-        and upper_shadow > 1.5 * lower_shadow
-        and shadow_size > 3 * (close - high)
+        close > open
+        and body_size < 0.02
+        and lower_shadow > 2 * upper_shadow
+        and shadow_size > 3 * body_size
     )
 
 
-def bull_spinning_top_bearish_followup(
+def spinning_top_bearish_followup(
     minute1: Tuple[float, float, float, float],
     minute2: Tuple[float, float, float, float],
 ) -> bool:
     return (
-        bull_spinning_top(minute1[0], minute1[1], minute1[2], minute1[3])
+        spinning_top(minute1[0], minute1[1], minute1[2], minute1[3])
         and minute2[0] > minute2[3]
     )
+
+
+def bullish_candle_followed_by_dragonfly(
+    minute1: Tuple[float, float, float, float],
+    minute2: Tuple[float, float, float, float],
+) -> bool:
+    return bullish_candle(
+        minute1[0], minute1[1], minute1[2], minute1[3]
+    ) and bull_dragonfly_candle(minute2[0], minute2[1], minute2[2], minute2[3])
