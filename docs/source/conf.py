8974d13949acda24879e5faf57e59cf0d661c12f
Amichay Oren <amichay@appcard.com>
2020-08-16 20:58:13 -0400

    debugging

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,6 @@ author = "amor71"
 # The full version, including alpha/beta/rc tags
 release = "0.0.1"
 
-master_doc = "index"
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -53,3 +52,5 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+master_doc = "index"
