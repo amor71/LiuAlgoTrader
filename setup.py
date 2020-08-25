61f36efed54571768721ca67eb0a37f7c51bc665
Amichay Oren <amichay@appcard.com>
2020-08-25 22:10:47 +0300

    add travis deploy to pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="liualgotrader-amor71",
+    name="liualgotrader",
     version="0.0.1",
     author="amor71",
     author_email="amichay@sgeltd.com",
