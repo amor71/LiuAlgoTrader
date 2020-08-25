c8d7f3ceef02b603ff426fb5454e27747c86ec41
Amichay Oren <amichay@appcard.com>
2020-08-25 23:36:40 +0300

    setup

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 import setuptools
 
+from version import get_git_version
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
 setuptools.setup(
     name="liualgotrader",
-    version="0.0.3",
+    version=get_git_version(),
     author="amor71",
     author_email="amichay@sgeltd.com",
     description="a Pythonic all-batteries-included framework for effective algorithmic trading. The framework is intended to simplify development, testing, deployment and evaluating algo trading strategies.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/amor71/LiuAlgoTrader",
+    license="MIT",
+    install_requires=requirements,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
