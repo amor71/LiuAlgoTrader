e1b0b7ae35f981a50d07d719f8d574038026661c
Amichay Oren <amichay@appcard.com>
2020-08-28 13:53:41 +0300

    move doc to latest

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, amor71"
 author = "amor71"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.34"
+release = "0.0.35"
 
 # -- General configuration ---------------------------------------------------
 
