f8a45d1891060a7b341eb7f05eab2eabeb9a9c63
Amichay Oren <amichay@sgeltd.com>
2020-09-14 21:02:06 +0300

    fix

--- liualgotrader/consumer.py
+++ liualgotrader/consumer.py
@@ -592,7 +592,6 @@ async def handle_data_queue_msg(
                         )
                         trading_data.last_used_strategy[symbol] = s
                         if what["side"] == "buy":
-
                             trading_data.buy_time[symbol] = datetime.now(
                                 tz=timezone("America/New_York")
                             ).replace(second=0, microsecond=0)
