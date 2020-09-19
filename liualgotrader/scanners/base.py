b4ccf68371a044d6802660842313fefae5c85721
Amichay Oren <amichay@sgeltd.com>
2020-09-19 08:45:10 -0400

    fix samples, make scanner base class abstract

--- liualgotrader/scanners/base.py
+++ liualgotrader/scanners/base.py
@@ -1,22 +1,24 @@
-from datetime import datetime, timedelta
+from abc import ABCMeta, abstractmethod
+from datetime import timedelta
 from typing import List, Optional
 
 import alpaca_trade_api as tradeapi
 
 
-class Scanner:
+class Scanner(metaclass=ABCMeta):
     def __init__(
         self,
         name: str,
+        data_api: tradeapi,
         recurrence: Optional[timedelta],
         target_strategy_name: Optional[str],
-        data_api: tradeapi,
     ):
         self.name = name
         self.recurrence = recurrence
         self.target_strategy_name = target_strategy_name
         self.data_api = data_api
 
+    @abstractmethod
     async def run(self) -> List[str]:
         return []
 
