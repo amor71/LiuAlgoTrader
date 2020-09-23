dfbbafc18889bcafd587b4696f5c4f364cf77530
Amichay Oren <amichay@sgeltd.com>
2020-09-22 22:17:25 -0400

    version cleanup

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-import setuptools
+import codecs
+import os.path
 
-from version import get_git_version
+import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -9,9 +10,24 @@ with open("liualgotrader/requirements.txt") as f:
     requirements = f.read().splitlines()
 
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 setuptools.setup(
     name="liualgotrader",
-    version=get_git_version(),
+    version=get_version("liualgotrader/__init__.py"),
     author="amor71",
     author_email="amichay@sgeltd.com",
     description="a Pythonic all-batteries-included framework for effective algorithmic trading. The framework is intended to simplify development, testing, deployment and evaluating algo trading strategies.",
