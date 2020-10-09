25f4104d7f7690ee62d138118f929e54515af676
Amichay Oren <amichay@sgeltd.com>
2020-10-09 18:46:30 -0400

    add support for setting the number of minutes to liquidate before end of the trading day - via the tradeplan file

--- liualgotrader/consumer.py
+++ liualgotrader/consumer.py
@@ -881,6 +881,8 @@ def consumer_main(
     config.portfolio_value = conf.get("portfolio_value", None)
     if "risk" in conf:
         config.risk = conf["risk"]
+    if "market_liquidation_end_time_minutes" in conf:
+        config.market_liquidation_end_time_minutes = conf["market_liquidation_end_time_minutes"]
 
     market_data.minute_history = minute_history
     try:
