d8c84272dedf1feb88c23f5e61f9ff165a28fd2e
Amichay Oren <amichay@sgeltd.com>
2020-11-13 13:21:43 -0500

    remove 3.9 support until streamlit issues are resolved
    Signed-off-by: Amichay Oren <amichay@sgeltd.com>

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        #        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
