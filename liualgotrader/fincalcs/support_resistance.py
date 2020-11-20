d356d1de652e19bdbb2860046bdba271575cff5c
Amichay Oren <amichay@sgeltd.com>
2020-11-20 21:36:45 +0200

    improve support-level calculations
    Signed-off-by: Amichay Oren <amichay@sgeltd.com>

--- liualgotrader/fincalcs/support_resistance.py
+++ liualgotrader/fincalcs/support_resistance.py
@@ -5,18 +5,21 @@ from typing import List, Optional
 import numpy as np
 import pytz
 from pandas import DataFrame as df
-from pandas import DatetimeIndex
 from pandas import Timestamp as ts
 
 from liualgotrader.common import config
 from liualgotrader.common.tlog import tlog
 
+est = pytz.timezone("US/Eastern")
+
 
 class StopRangeType(Enum):
     LAST_100_MINUTES = 1
-    DAILY = 2
-    WEEKLY = 3
-    DATE_RANGE = 4
+    LAST_2_HOURS = 2
+    LAST_3_HOURS = 3
+    DAILY = 10
+    WEEKLY = 20
+    DATE_RANGE = 50
 
 
 def grouper(iterable):
@@ -81,39 +84,46 @@ async def find_resistances(
     return None
 
 
-async def find_supports(
-    symbol: str,
-    strategy_name: str,
-    current_value: float,
-    minute_history: df,
-    debug=False,
-) -> List[float]:
-    """calculate supports"""
-    for back_track_min in range(120, len(minute_history.index), 60):
+def find_supports(
+    current_value,
+    minute_history,
+    now: datetime,
+    range_type: StopRangeType = StopRangeType.LAST_100_MINUTES,
+):
+    # get low Series based on select time-range
+    if range_type == StopRangeType.DAILY:
         series = (
-            minute_history["close"][-back_track_min:]
+            minute_history["low"][
+                ts(
+                    now.replace(
+                        hour=9, minute=30, second=0, microsecond=0, tzinfo=est
+                    )
+                ) :
+            ]
             .dropna()
-            .between_time("9:30", "16:00")
-            .resample("15min")
+            .resample("5min")
             .min()
-        ).dropna()
-        diff = np.diff(series.values)
-        high_index = np.where((diff[:-1] <= 0) & (diff[1:] > 0))[0] + 1
-        if len(high_index) > 0:
-            local_maximas = sorted(
-                [series[i] for i in high_index if series[i] <= current_value]
-            )
-            if len(local_maximas) > 0:
-                if debug:
-                    tlog("find_supports()")
-                    tlog(f"{minute_history}")
-                    tlog(f"{minute_history['close'][-1]}, {series}")
-                # tlog(
-                #    f"[{strategy_name}] find_supports({symbol})={local_maximas}"
-                # )
-                return local_maximas
+        )
+    elif range_type == StopRangeType.LAST_100_MINUTES:
+        series = minute_history["low"][-100:].dropna().resample("5min").min()
+        series = series[ts(now).floor("1D") :]
+    elif range_type == StopRangeType.LAST_2_HOURS:
+        series = minute_history["low"][-120:].dropna().resample("5min").min()
+        series = series[ts(now).floor("1D") :]
+    elif range_type == StopRangeType.LAST_3_HOURS:
+        series = minute_history["low"][-180:].dropna().resample("5min").min()
+        series = series[ts(now).floor("1D") :]
+    else:
+        raise NotImplementedError(
+            f"stop-range type {range_type} is not implemented"
+        )
 
-    return []
+    # find local minima
+    diff = np.diff(series.values)
+    low_index = np.where((diff[:-1] <= 0) & (diff[1:] > 0))[0] + 1
+    if len(low_index) > 0:
+        return [series[x] for x in low_index if series[x] < current_value]
+    return None
 
 
 def find_stop(
@@ -122,7 +132,6 @@ def find_stop(
     now: datetime,
     range_type: StopRangeType = StopRangeType.LAST_100_MINUTES,
 ):
-    """calculate stop loss"""
     if range_type in (StopRangeType.DATE_RANGE, StopRangeType.WEEKLY):
         raise NotImplementedError(
             f"stop-range type {range_type} is not implemented"
@@ -131,8 +140,10 @@ def find_stop(
     if range_type == StopRangeType.DAILY:
         series = (
             minute_history["low"][
-                DatetimeIndex(
-                    now.replace(hour=9, minute=30, second=0, microsecond=0)
+                ts(
+                    now.replace(
+                        hour=9, minute=30, second=0, microsecond=0, tzinfo=est
+                    )
                 ) :
             ]
             .dropna()
@@ -141,7 +152,7 @@ def find_stop(
         )
     else:
         series = minute_history["low"][-100:].dropna().resample("5min").min()
-        series = series[DatetimeIndex(now).floor("1D") :]
+        series = series[ts(now).floor("1D") :]
 
     diff = np.diff(series.values)
     low_index = np.where((diff[:-1] <= 0) & (diff[1:] > 0))[0] + 1
