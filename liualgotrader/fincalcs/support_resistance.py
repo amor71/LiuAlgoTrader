f6dd4c6e0ed2c8a6a7c52a9f2e115a8ae25f7503
Amichay Oren <amichay@sgeltd.com>
2020-11-20 13:04:09 +0200

    evolve find_stop function

--- liualgotrader/fincalcs/support_resistance.py
+++ liualgotrader/fincalcs/support_resistance.py
@@ -1,13 +1,22 @@
 from datetime import datetime, timedelta
+from enum import Enum
 from typing import List, Optional
 
 import numpy as np
 import pytz
 from pandas import DataFrame as df
+from pandas import DatetimeIndex
 from pandas import Timestamp as ts
 
 from liualgotrader.common import config
 from liualgotrader.common.tlog import tlog
+
+
+class StopRangeType(Enum):
+    LAST_100_MINUTES = 1
+    DAILY = 2
+    WEEKLY = 3
+    DATE_RANGE = 4
 
 
 def grouper(iterable):
@@ -17,7 +26,9 @@ def grouper(iterable):
 
         if (
             not prev
-            or -config.group_margin <= float(item - prev) / prev <= config.group_margin
+            or -config.group_margin
+            <= float(item - prev) / prev
+            <= config.group_margin
         ):
             group.append(item)
         else:
@@ -39,7 +50,9 @@ async def find_resistances(
 
     est = pytz.timezone("America/New_York")
     back_time = ts(datetime.now(est)).to_pydatetime() - timedelta(days=3)
-    back_time_index = minute_history["close"].index.get_loc(back_time, method="nearest")
+    back_time_index = minute_history["close"].index.get_loc(
+        back_time, method="nearest"
+    )
 
     series = (
         minute_history["close"][back_time_index:]
@@ -103,12 +116,35 @@ async def find_supports(
     return []
 
 
-def find_stop(current_value, minute_history, now):
+def find_stop(
+    current_value,
+    minute_history,
+    now: datetime,
+    range_type: StopRangeType = StopRangeType.LAST_100_MINUTES,
+):
     """calculate stop loss"""
-    series = minute_history["low"][-100:].dropna().resample("5min").min()
-    series = series[now.floor("1D") :]
+    if range_type in (StopRangeType.DATE_RANGE, StopRangeType.WEEKLY):
+        raise NotImplementedError(
+            f"stop-range type {range_type} is not implemented"
+        )
+
+    if range_type == StopRangeType.DAILY:
+        series = (
+            minute_history["low"][
+                DatetimeIndex(
+                    now.replace(hour=9, minute=30, second=0, microsecond=0)
+                ) :
+            ]
+            .dropna()
+            .resample("5min")
+            .min()
+        )
+    else:
+        series = minute_history["low"][-100:].dropna().resample("5min").min()
+        series = series[DatetimeIndex(now).floor("1D") :]
+
     diff = np.diff(series.values)
     low_index = np.where((diff[:-1] <= 0) & (diff[1:] > 0))[0] + 1
     if len(low_index) > 0:
-        return series[low_index[-1]] - max(0.05, current_value * 0.02)
-    return current_value * config.default_stop
+        return series[low_index[-1]]  # - max(0.05, current_value * 0.02)
+    return None  # current_value * config.default_stop
