94ef9303a972688dc77bc9e886b151b5f521e0f9
Amichay Oren <amichay@sgeltd.com>
2020-12-15 13:29:22 +0200

    forwarding version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, amor71"
 author = "amor71"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.67"
+release = "0.0.81"
 
 # -- General configuration ---------------------------------------------------
 
