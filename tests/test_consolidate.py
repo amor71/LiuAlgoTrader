e0517a0f340bda4573dc77a8bfd57af96fe6988d
Amichay Oren <amichay@sgeltd.com>
2020-12-15 09:35:17 +0200

    applying changes to travis pytest usage

--- tests/test_consolidate.py
+++ tests/test_consolidate.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import pytest
 from hypothesis import example, given, settings
 from hypothesis.strategies import text
@@ -8,10 +10,12 @@ from liualgotrader.analytics.consolidate import trades
 @settings(deadline=None, max_examples=5)  # type: ignore
 @given(text())
 @pytest.mark.asyncio
+@pytest.mark.devtest
 async def test_trades(batch_id: str) -> None:
     await trades(batch_id)
 
 
 @pytest.mark.asyncio
+@pytest.mark.devtest
 async def test_trades_specific_batch() -> None:
     await trades("89177ae2-a459-4614-a2bc-474f1e0b7c89")
