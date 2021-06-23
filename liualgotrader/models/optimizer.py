3d165125bed83c05984ed632acffb102313df58e
Amichay Oren <amichay@sgeltd.com>
2021-06-23 11:42:21 +0300

    analysis: wip

--- liualgotrader/models/optimizer.py
+++ liualgotrader/models/optimizer.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import asyncpg
 
 from liualgotrader.common import config
@@ -21,3 +23,30 @@ class OptimizerRun:
                     optimizer_session_id,
                     batch_id,
                 )
+
+    @classmethod
+    async def get_portfolio_ids(
+        cls,
+        optimizer_session_id: str,
+    ) -> List[str]:
+        try:
+            pool = config.db_conn_pool
+        except AttributeError:
+            await create_db_connection()
+            pool = config.db_conn_pool
+
+        async with config.db_conn_pool.acquire() as con:
+            recrods = await con.fetch(
+                """
+                    SELECT 
+                        p.portfolio_id
+                    FROM 
+                        optimizer_run as o, portfolio_batch_ids as p
+                    WHERE
+                        o.batch_id = p.batch_id
+                        AND optimizer_session_id = $1;
+                """,
+                optimizer_session_id,
+            )
+
+        return [row[0] for row in recrods]
