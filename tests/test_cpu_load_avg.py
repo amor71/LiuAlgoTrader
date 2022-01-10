a65a9e9edbc7d7bdea11051c58a8401e16b506df
Jonathan Morland-Barrett <jmorland.barrett@gmail.com>
2022-01-10 10:42:39 +0800

    Change interval of psutil.cpu_percent to None to check performance since last call.

--- tests/test_cpu_load_avg.py
+++ tests/test_cpu_load_avg.py
@@ -6,4 +6,4 @@ import os
 
 @pytest.mark.devtest
 def test_load_cpu_avg():
-    assert psutil.cpu_percent(interval=1) / psutil.cpu_count() >= 0.0
+    assert psutil.cpu_percent(interval=None) / psutil.cpu_count() >= 0.0
