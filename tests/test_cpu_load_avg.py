4dd6599e40e19b4c182968795d4492a2ac7de391
Jonathan Morland-Barrett <jmorland.barrett@gmail.com>
2022-01-10 10:21:11 +0800

    Update trader program to use cpu_percent and cpu_count from psutil package. Tested with both Linux and Windows. Update test to match code.

--- tests/test_cpu_load_avg.py
+++ tests/test_cpu_load_avg.py
@@ -1,15 +1,9 @@
 import pytest
 import psutil
+import os
+
+
 
 @pytest.mark.devtest
-def test_one_minute():
-    assert psutil.getloadavg()[0] >= 0.0
-    
-@pytest.mark.devtest
-def test_five_minute():
-    assert psutil.getloadavg()[1] >= 0.0
-
-    
-@pytest.mark.devtest
-def test_fifteen_minute():
-    assert psutil.getloadavg()[2] >= 0.0
+def test_load_cpu_avg():
+    assert psutil.cpu_percent(interval=1) / psutil.cpu_count() >= 0.0
