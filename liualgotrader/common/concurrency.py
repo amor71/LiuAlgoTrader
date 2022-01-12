fcaedcdc7b33c47869c602a5df969050f8216868
Amichay Oren <amichay@sgeltd.com>
2022-01-12 18:56:41 +0200

    improve calculation of number of consumer processes

---
+++ liualgotrader/common/concurrency.py
@@ -0,0 +1,24 @@
+import math
+
+import psutil
+
+from liualgotrader.common import config
+from liualgotrader.common.tlog import tlog
+
+
+def calc_num_consumer_processes() -> int:
+    if config.num_consumers > 0:
+        return config.num_consumers
+
+    num_cpu = psutil.cpu_count(False)
+
+    if not num_cpu:
+        raise AssertionError(
+            "Can't automatically detect number of CPUs, use fixed configuration"
+        )
+
+    load_pct: float = psutil.cpu_percent(interval=5)
+
+    tlog(f"Total CPU Load:{load_pct}, num_cpu:{num_cpu}")
+
+    return int(5.0 * (1 - load_pct / 100.0) * num_cpu)
--- tests/test_cpu_load_avg.py
+++ tests/test_cpu_load_avg.py
@@ -1,9 +1,51 @@
-import pytest
-import psutil
 import os
+import time
+
+import psutil
+import pytest
+
+from liualgotrader.common import config
+from liualgotrader.common.concurrency import calc_num_consumer_processes
 
 
-
-@pytest.mark.devtest
 def test_load_cpu_avg():
-    assert psutil.cpu_percent(interval=None) / psutil.cpu_count() >= 0.0
+    print("test_load_cpu_avg")
+    cpu_load = psutil.cpu_percent(interval=5)
+    num_cpus = psutil.cpu_count()
+
+    print(f"cpu_load = {cpu_load}, num_cpus={num_cpus}")
+
+    if cpu_load <= 0 or num_cpus <= 0:
+        raise AssertionError("Unexpected result")
+
+
+def test_fix_num_consumer_processes():
+    print("test_fix_num_consumer_processes")
+    possible_values = [5, 8, 500]
+
+    for num_consumers in possible_values:
+        config.num_consumers = num_consumers
+        result = calc_num_consumer_processes()
+        if result != num_consumers:
+            raise AssertionError(
+                f"calc_num_consumer_processes() - {result} returned, while expected {num_consumers}"
+            )
+
+    return True
+
+
+def test_calc_num_consumer_processes():
+    print("test_calc_num_consumer_processes")
+
+    for _ in range(20):
+        config.num_consumers = 0
+        result = calc_num_consumer_processes()
+        print(f"calc_num_consumer_processes() -> {result}")
+        if result <= 0:
+            raise AssertionError(
+                f"calc_num_consumer_processes() - {result} unexpected"
+            )
+
+        time.sleep(10)
+
+    return True
