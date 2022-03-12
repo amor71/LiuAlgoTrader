8a811d7ca3a6c836603764619ce511c669dd661e
Amichay Oren <amichay@sgeltd.com>
2022-03-12 03:50:23 +0200

    complete tradier trading calendar implementation & tests

--- tests/test_tradier_trader.py
+++ tests/test_tradier_trader.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import date, timedelta
 
 import pytest
 
@@ -59,7 +60,7 @@ async def test_is_fractionable():
         print("APA is not fractionable")
     aapl_fractionable = await tradier_trader.is_fractionable("AAPL")
     if not aapl_fractionable:
-        print("AAPL is fractionable ")
+        print("AAPL is not fractionable ")
 
     return True
 
@@ -68,7 +69,50 @@ async def test_is_fractionable():
 @pytest.mark.devtest
 async def test_get_market_schedule():
     print("test_get_market_schedule")
-    open, close = await tradier_trader.get_market_schedule()
-    print(f"market open {open} close {close}")
+    start, end = tradier_trader.get_market_schedule()
+    print(f"market open {start} close {end}")
 
+    return True
+
+
+@pytest.mark.asyncio
+@pytest.mark.devtest
+async def test_is_market_open_today():
+    print("test_is_market_open_today")
+    is_open = tradier_trader.is_market_open_today()
+    print(f"Market open today {is_open}")
+
+    return True
+
+
+@pytest.mark.asyncio
+@pytest.mark.devtest
+async def test_get_time_market_close():
+    print("test_get_time_market_close")
+    when_close = tradier_trader.get_time_market_close()
+    print(f"Market close today {when_close}")
+
+    return True
+
+
+@pytest.mark.asyncio
+@pytest.mark.devtest
+async def test_get_trading_days():
+    print("test_get_trading_days")
+    td = tradier_trader.get_trading_days(
+        start_date=date.today(), end_date=date.today()
+    )
+    print(f"trading days {td}")
+
+    print("test_get_trading_days")
+    td = tradier_trader.get_trading_days(
+        start_date=date.today() - timedelta(days=5), end_date=date.today()
+    )
+    print(f"trading days {td}")
+
+    print("test_get_trading_days")
+    td = tradier_trader.get_trading_days(
+        start_date=date.today() - timedelta(days=65), end_date=date.today()
+    )
+    print(f"trading days {td}")
     return True
